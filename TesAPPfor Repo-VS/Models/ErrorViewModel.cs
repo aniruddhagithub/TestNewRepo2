3801e91ac743809853a4290305ce1ad035462ba9
aniruddhagithub <aniruddha.deshmukh07@gmail.com>
2023-05-12 12:03:25 +0530

    first commit

---
+++ TesAPPfor Repo-VS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesAPPfor_Repo_VS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
